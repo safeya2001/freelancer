4b69c153c994a731bd5a41a3ebec5e7ab44bab0d
Safeya <s.Isteit@dopatravelapp.com>
2026-03-25 13:32:30 +0300

    Fix migration script: proper process exit after completion

--- backend/scripts/run-migrations.js
+++ backend/scripts/run-migrations.js
@@ -35,4 +35,6 @@ async function runMigrations() {
   console.log('Migrations complete.');
 }
 
-runMigrations();
+runMigrations()
+  .then(() => process.exit(0))
+  .catch((err) => { console.error(err); process.exit(1); });
